f00ccffdb7fa3342d67a3ad981c14628e82350ea
ASjet <Aryan.ITFS@iCloud.com>
2021-04-15 09:48:13 +0800

    fix(setupCVEnv.sh):fix condition expression

--- setupCVEnv.sh
+++ setupCVEnv.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Build Type
 build_type=RELEASE
@@ -7,7 +7,7 @@ build_type=RELEASE
 cmake_install_dir=/usr/local
 
 # Python3 executable path
-py3_exe_path=/usr/bin/python3
+py3_exec_path=/usr/bin/python3
 
 # Python3 include directory
 py3_include_dir=/usr/include/python3.8
@@ -36,22 +36,33 @@ then
     sudo apt install libgtk2.0-dev -y
     sudo apt install libatlas-base-dev gfortran -y
     echo "========================================================================"
-    echo "Warning: Configure may wrong in different target platform"
-    echo "Edit variables in setupCVEnv.sh"
+    echo "Warning: Configuration may wrong in different target platform"
+    echo "Configure environment variables in setupCVEnv.sh"
+    echo "==> Default"
+    echo "bulid_type=$build_type"
+    echo "cmake_install_dir=$cmake_install_dir"
+    echo "py3_exec_path=$py3_exec_path"
+    echo "py3_include_dir=$py3_include_dir"
+    echo "py3_lib_dir=$py3_lib_dir"
+    echo "np_include_dir=$np_include_dir"
+    echo "<=="
     echo "Increase vitual memory by editing /etc/dphys-swapfile CONF_SWAPSIZE=4096"
     echo "Then rerun this script"
-elif [ ! -e ./.BUILD_FLAG ]
+elif [[ -e ./.ENV_FLAG && ! -e ./.BUILD_FLAG ]]
 then
     touch ./.BUILD_FLAG
     git clone git://github.com/opencv/opencv ~/opencv
     mkdir ~/opencv/build
     cd ~/opencv/build
-    cmake -D CMAKE_BUILD_TYPE=$build_type -D CMAKE_INSTALL_PREFIX=$cmake_install_dir -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -DCMAKE_SHARED_LINKER_FLAGS='-latomic' -D WITH_LIBV4L=ON PYTHON3_EXECUTABLE=$py3_exe_path PYTHON_INCLUDE_DIR=$py3_include_dir PYTHON_LIBRARY=$py3_lib_dir PYTHON3_NUMPY_INCLUDE_DIRS=$np_include_dir ..
+    cmake -D CMAKE_BUILD_TYPE=$build_type -D CMAKE_INSTALL_PREFIX=$cmake_install_dir -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -DCMAKE_SHARED_LINKER_FLAGS='-latomic' -D WITH_LIBV4L=ON PYTHON3_EXECUTABLE=$py3_exec_path PYTHON_INCLUDE_DIR=$py3_include_dir PYTHON_LIBRARY=$py3_lib_dir PYTHON3_NUMPY_INCLUDE_DIRS=$np_include_dir ..
     echo "========================================================================"
     cat /proc/cpuinfo | grep processor
-    echo "change thread_num in this script"
+    echo "Configure thread_num in setupCVEnv.sh"
+    echo "==> Default"
+    echo "thread_num=$thread_num"
+    echo "<=="
     echo "Then rerun this script"
-elif [ ! -e ./.INSTALL_FLAG ]
+elif [[ -e ./.ENV_FLAG && -e ./.BUILD_FLAG && ! -e ./.INSTALL_FLAG ]]
 then
     touch ./.INSTALL_FLAG
     cd ~/opencv/build

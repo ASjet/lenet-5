952dde6ec97aa6cad6129dc9967689eaa037a0c9
ASjet <Aryan.ITFS@iCloud.com>
2021-04-16 21:54:30 +0800

    feat(camera.py):load ip module

--- camera.py
+++ camera.py
@@ -6,15 +6,26 @@ import ip
 camera_id = 0
 
 cap = cv2.VideoCapture(camera_id)
-
-while(cap.isOpened() == True):
+while(cap.isOpened()):
     [ret,frame] = cap.read()
     if(ret == True):
         # Origin captured frame
         cv2.imshow("Camera",frame)
 
         # Processed frame
-        output = ip.process(frame)
-        cv2.imshow("Output",output)
+        sel = ip.cut(frame,256)
+        dgt = ip.getDigit(sel)
+        bd = ip.bold(dgt)
+        flag,roi = ip.getROI(bd)
+        if(flag == False):
+            continue
+        x_size = cv2.resize(roi, (28,28), cv2.WARP_FILL_OUTLIERS)
+        output = x_size
+
+        cv2.imshow("Captured",bd)
+        cv2.imshow("NNinput",x_size)
+        key = cv2.waitKey(1)
+        if(key == 27):
+            break
 
 cap.release()

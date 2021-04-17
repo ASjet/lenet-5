075be7a7035733c730ccb283798a747c4c8ff714
ASjet <Aryan.ITFS@iCloud.com>
2021-04-17 13:14:38 +0800

    feat(DigitDetect.py sp.py):link NN

---
+++ DigitDetect.py
@@ -0,0 +1,44 @@
+from cv2 import cv2
+import network
+import ip
+
+# Set VideoCaptureDeviceID
+camera_id = 0
+
+# Set model folder path
+model_folder_path = "nn_model/"
+
+# Select NN model
+model_name = "model_acc9779.json"
+
+
+net = network.load(model_folder_path+model_name)
+cap = cv2.VideoCapture(camera_id)
+
+while(cap.isOpened()):
+    [ret,frame] = cap.read()
+    if(ret == True):
+        # Origin captured frame
+        # cv2.imshow("Origin",frame)
+
+        # Processed frame
+        sel = ip.cut(frame,256)
+        dgt = ip.getDigit(sel)
+        flag,roi = ip.getROI(dgt)
+        if(flag == False):
+            continue
+        target = cv2.resize(roi, (28,28), cv2.WARP_FILL_OUTLIERS)
+        input_layer = target.resize(784,1)
+
+        cv2.imshow("Captured",sel)
+        cv2.imshow("Filter",dgt)
+        cv2.imshow("ROI",roi)
+
+        input_layer[input_layer > 0] = 1
+        print(net.feedforward(input_layer))
+
+        key = cv2.waitKey(1)
+        if(key == 27): # Type ESC to break
+            break
+
+cap.release()

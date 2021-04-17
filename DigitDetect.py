3b9be3138b350dc7807684401ab2ccdd4b8c4756
ASjet <Aryan.ITFS@iCloud.com>
2021-04-17 13:38:50 +0800

    feat(DigitDetect.py):optimize display

--- DigitDetect.py
+++ DigitDetect.py
@@ -28,17 +28,18 @@ while(cap.isOpened()):
         if(flag == False):
             continue
         target = cv2.resize(roi, (28,28), cv2.WARP_FILL_OUTLIERS)
-        input_layer = target.resize(784,1)
+        input_layer = target.reshape(784,1)
 
         cv2.imshow("Captured",sel)
         cv2.imshow("Filter",dgt)
-        cv2.imshow("ROI",roi)
+        cv2.imshow("ROI",target)
 
         input_layer[input_layer > 0] = 1
-        print(net.feedforward(input_layer))
+        print('\rNum: ',net.feedforward(input_layer),sep='',end='')
 
         key = cv2.waitKey(1)
         if(key == 27): # Type ESC to break
             break
 
 cap.release()
+print()

5e0f8ddad1cf857deaf641b239f7cc3dbbd213a0
ASjet <Aryan.ITFS@iCloud.com>
2021-04-18 18:40:35 +0800

    refactor:optimization

--- DigitDetect.py
+++ DigitDetect.py
@@ -5,22 +5,13 @@ import ip
 # Set VideoCaptureDeviceID
 camera_id = 0
 
-# Set model folder path
-model_folder_path = "nn_model/"
 
-# Select NN model
-model_name = "model_acc9779.json"
-
-
-net = network.load(model_folder_path+model_name)
+net = network.load()
 cap = cv2.VideoCapture(camera_id)
 
 while(cap.isOpened()):
     [ret,frame] = cap.read()
     if(ret == True):
-        # Origin captured frame
-        # cv2.imshow("Origin",frame)
-
         # Processed frame
         sel = ip.cut(frame,256)
         dgt = ip.getDigit(sel)
@@ -30,12 +21,13 @@ while(cap.isOpened()):
         target = cv2.resize(roi, (28,28), cv2.WARP_FILL_OUTLIERS)
         input_layer = target.reshape(784,1)
 
+        # Display
+        # cv2.imshow("Origin",frame)
         cv2.imshow("Captured",sel)
         cv2.imshow("Filter",dgt)
         cv2.imshow("ROI",target)
 
-        input_layer[input_layer > 0] = 1
-        print('\rNum: ',net.feedforward(input_layer),sep='',end='')
+        print('\rNum: ',net.feedforward(input_layer),sep='',end=' ')
 
         key = cv2.waitKey(1)
         if(key == 27): # Type ESC to break
